519e952e47a011eef7fbd74b233e86cc027d2165
jean <jean.haderer@keyconsulting.fr>
2022-05-25 11:19:30 +0200

    multiple workflow yaml

--- poc-protools/poc-protools-workflow/src/main/java/fr/insee/metallica/workflow/service/WorkflowConfigurationService.java
+++ poc-protools/poc-protools-workflow/src/main/java/fr/insee/metallica/workflow/service/WorkflowConfigurationService.java
@@ -8,6 +8,9 @@ import java.util.stream.Collectors;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
@@ -18,14 +21,25 @@ public class WorkflowConfigurationService {
 	private Map<String, WorkflowDescriptor> workflows;
 	private Map<UUID, WorkflowDescriptor> workflowsByUuid;
 	
+	@Value("classpath:workflows-*.yaml")
+	private Resource[] workflowsYaml;
+	
 	@PostConstruct
 	public void initialize() throws IOException {
 		var mapper = new ObjectMapper(new YAMLFactory());
 		try(var stream = getClass().getResourceAsStream("/workflows.yaml")) {
-			var workflows = mapper.readValue(stream, WorkflowsProperties.class);
-			this.workflows = workflows.toWorkflowDescriptors();
-			this.workflowsByUuid = this.workflows.values().stream().collect(Collectors.toMap(WorkflowDescriptor::getId, w -> w));
+			if (stream != null) {
+				var workflows = mapper.readValue(stream, WorkflowsProperties.class);
+				this.workflows = workflows.toWorkflowDescriptors();
+			}
 		}
+		for (var resource : workflowsYaml) {
+			try (var stream = resource.getInputStream()) {
+				var workflows = mapper.readValue(resource.getInputStream(), WorkflowsProperties.class);
+				this.workflows.putAll(workflows.toWorkflowDescriptors());
+			}
+		}
+		this.workflowsByUuid = this.workflows.values().stream().collect(Collectors.toMap(WorkflowDescriptor::getId, w -> w));
 	}
 	
 	public WorkflowDescriptor getWorkflow(String name) {
